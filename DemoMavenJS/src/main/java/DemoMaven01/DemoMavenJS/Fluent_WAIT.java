09830d50e6ddda7576c8158fcd86190122bcec7b
DhanusheswaranM <mdhanusheswaran@gmail.com>
2025-03-11 14:55:00 +0530

    new class

---
+++ DemoMavenJS/src/main/java/DemoMaven01/DemoMavenJS/Fluent_WAIT.java
@@ -0,0 +1,36 @@
+package DemoMaven01.DemoMavenJS;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+public class Fluent_WAIT {
+
+	public static void main(String[] args) {
+		WebDriver dvr = new ChromeDriver();
+		dvr.manage().window().maximize();
+		dvr.get("https://www.rediff.com/");
+		dvr.findElement(By.xpath("//div[@class=\"toplinks\"]//a[2]")).click();
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(dvr)
+				.withTimeout(Duration.ofSeconds(15))
+				.pollingEvery(Duration.ofSeconds(5))
+				.ignoring(NoSuchElementException.class);
+		WebElement BSE = wait.until(new Function<WebDriver , WebElement>(){
+			public WebElement apply(WebDriver dvr) {
+				return dvr.findElement(By.cssSelector(".hmbseindicestable>ul:nth-child(2)>li:nth-child(2)"));
+			}
+			
+		});
+		String msg = BSE.getText();
+		System.out.println(msg);
+		dvr.close();
+	}
+
+}
--- DemoMavenJS/src/main/java/DemoMaven01/DemoMavenJS/Synchronisation_explicit.java
+++ DemoMavenJS/src/main/java/DemoMaven01/DemoMavenJS/Synchronisation_explicit.java
@@ -13,18 +13,33 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Synchronisation_explicit {
 		public static void main(String[] args) throws InterruptedException {
 			WebDriver drive = new ChromeDriver();
-			WebDriverWait wait = new WebDriverWait(drive,Duration.ofSeconds(5));
+			drive.manage().window().maximize();
+			WebDriverWait wait = new WebDriverWait(drive,Duration.ofSeconds(10));
 //			drive.get("https://omayo.blogspot.com/");
 //			WebElement timerbtn = wait(ExpectedConditions.visibilityOfElementLocated((drive.findElement(By.cssSelector("#timerButton")))));
 //			timerbtn.click();
 //			
-			drive.get("https://www.selenium.dev/selenium/web/dynamic.html");
-			drive.manage().window().maximize();
-			WebElement reveal = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#reveal")));
-			reveal.click();
-			WebElement revealtext = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#revealed")));
-			revealtext.sendKeys("HI"+Keys.ENTER);
-			System.out.println("Text of reveal text box : "+revealtext.getDomProperty("value"));
+//			drive.get("https://www.selenium.dev/selenium/web/dynamic.html");
+//			drive.manage().window().maximize();
+//			WebElement reveal = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#reveal")));
+//			reveal.click();
+//			WebElement revealtext = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#revealed")));
+//			revealtext.sendKeys("HI"+Keys.ENTER);
+//			System.out.println("Text of reveal text box : "+revealtext.getDomProperty("value"));
+//			drive.close();
+//			drive.get("https://configureselenium.blogspot.com/");
+//			WebElement selenium = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"PageList1\"]/div/div[1]/div[1]/div/ul/li[1]/a")));
+//			selenium.click();
+//			WebElement totalviews = drive.findElement(By.cssSelector("#Stats1_totalCount"));
+//			System.out.println(totalviews.getDomProperty("value"));
+			drive.get("https://configureselenium.blogspot.com/2020/11/selenium-tutorials.html");
+			WebElement totalviews = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#Stats1_totalCount")));
+			System.out.println("TotalViews : "+totalviews.getText());
+			WebElement selenium = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"PageList1\"]/div/div[1]/div[1]/div/ul/li[1]/a")));
+			selenium.click();
+			drive.navigate().refresh();
+			WebElement totalviews1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#Stats1_totalCount")));
+			System.out.println("TotalViews : "+totalviews1.getText());
 			drive.close();
 	}
 
