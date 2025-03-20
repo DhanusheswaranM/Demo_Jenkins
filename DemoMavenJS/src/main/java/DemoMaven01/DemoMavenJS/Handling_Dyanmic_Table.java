06cff2a435bbea29cb50d89e9c832919dae7faec
DhanusheswaranM <mdhanusheswaran@gmail.com>
2025-03-20 12:10:32 +0530

    19-03-2025

--- DemoMavenJS/src/main/java/DemoMaven01/DemoMavenJS/Handling_Dyanmic_Table.java
+++ DemoMavenJS/src/main/java/DemoMaven01/DemoMavenJS/Handling_Dyanmic_Table.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Handling_Dyanmic_Calender {
+public class Handling_Dyanmic_Table {
 
 	public static void main(String[] args) {
 		ChromeDriver dvr = new ChromeDriver();
